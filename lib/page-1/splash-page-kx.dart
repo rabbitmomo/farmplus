4c1915a4eedff4b9bf9a9055b1ecaadce1532f63
Kah Xuan <jarretlow123@gmail.com>
2024-03-12 17:39:46 +0800

    Replace the logo on splash screen

--- lib/page-1/splash-page-kx.dart
+++ lib/page-1/splash-page-kx.dart
@@ -15,12 +15,12 @@ class Scene extends StatelessWidget {
       child: TextButton(
         // splashpagekxnf1 (68:159)
         onPressed: () {},
-        style: TextButton.styleFrom (
+        style: TextButton.styleFrom(
           padding: EdgeInsets.zero,
         ),
         child: Container(
           width: double.infinity,
-          decoration: BoxDecoration (
+          decoration: BoxDecoration(
             color: Color(0xffffffff),
           ),
           child: Column(
@@ -28,40 +28,29 @@ class Scene extends StatelessWidget {
             children: [
               Container(
                 // autogroupp4pp5u1 (MusHcLMc7fLKhwhFWdp4PP)
-                padding: EdgeInsets.fromLTRB(92.5*fem, 164*fem, 91.5*fem, 66.5*fem),
+                padding: EdgeInsets.fromLTRB(
+                    92.5 * fem, 164 * fem, 91.5 * fem, 66.5 * fem),
                 width: double.infinity,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Container(
                       // designer11ctw (68:169)
-                      margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 17.5*fem),
-                      width: 135*fem,
-                      height: 154*fem,
+                      margin: EdgeInsets.fromLTRB(
+                          0 * fem, 0 * fem, 0 * fem, 17.5 * fem),
+                      width: 180 * fem,
+                      height: 200 * fem,
                       child: Image.asset(
-                        'assets/page-1/images/designer-1-1.png',
+                        'assets/page-1/images/SplashScreenLogo.png',
                       ),
-                    ),
-                    Center(
-                      // farmplusWjR (68:168)
-                      child: Text(
-                        'FarmPlus+',
-                        textAlign: TextAlign.center,
-                        style: SafeGoogleFont (
-                          'Poppins',
-                          fontSize: 32*ffem,
-                          fontWeight: FontWeight.w700,
-                          height: 1.5*ffem/fem,
-                        ),
-                      ),
-                    ),
+                    )
                   ],
                 ),
               ),
               Container(
                 // document1TPm (181:1129)
-                width: 384*fem,
-                height: 384*fem,
+                width: 384 * fem,
+                height: 384 * fem,
                 child: Image.asset(
                   'assets/page-1/images/document-1.png',
                   fit: BoxFit.cover,
@@ -71,6 +60,6 @@ class Scene extends StatelessWidget {
           ),
         ),
       ),
-          );
+    );
   }
 }
