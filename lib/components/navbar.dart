109e52e5246731e1cd1a86ec0f41bbc371963af2
Kah Xuan <jarretlow123@gmail.com>
2024-03-16 23:58:09 +0800

    Styling Profile Page

--- lib/components/navbar.dart
+++ lib/components/navbar.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-
 class Navbar extends StatefulWidget {
-  const Navbar({Key? key}) : super(key: key);
+  final int index;
+
+  const Navbar({Key? key, this.index = 0}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _Navbar();
 }
 
 class _Navbar extends State<Navbar> {
-  int _currentIndex = 0;
+  late int _currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentIndex = widget.index;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,37 +52,31 @@ class _Navbar extends State<Navbar> {
               break;
           }
         },
-        selectedItemColor:
-            const Color.fromARGB(255, 3, 48, 11), // Selected item color
-        unselectedItemColor:
-            Colors.grey.withOpacity(0.5), // Unselected item color
+        selectedItemColor: Color.fromARGB(255, 23, 116, 41), // Selected item color
+        unselectedItemColor: Colors.grey.withOpacity(0.5), // Unselected item color
         items: [
           BottomNavigationBarItem(
             icon: _currentIndex == 0
                 ? Icon(Icons.home) // Normal icon for selected item
-                : const Icon(
-                    Icons.home_outlined), // Outlined icon for unselected item
+                : const Icon(Icons.home_outlined), // Outlined icon for unselected item
             label: "Home",
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 1
                 ? Icon(Icons.shopping_bag) // Normal icon for selected item
-                : const Icon(Icons
-                    .shopping_bag_outlined), // Outlined icon for unselected item
+                : const Icon(Icons.shopping_bag_outlined), // Outlined icon for unselected item
             label: "Order",
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 2
                 ? Icon(Icons.grass) // Normal icon for selected item
-                : const Icon(
-                    Icons.grass_outlined), // Outlined icon for unselected item
+                : const Icon(Icons.grass_outlined), // Outlined icon for unselected item
             label: "Crop",
           ),
           BottomNavigationBarItem(
             icon: _currentIndex == 3
                 ? Icon(Icons.account_circle) // Normal icon for selected item
-                : const Icon(Icons
-                    .account_circle_outlined), // Outlined icon for unselected item
+                : const Icon(Icons.account_circle_outlined), // Outlined icon for unselected item
             label: "Profile",
           ),
         ],
