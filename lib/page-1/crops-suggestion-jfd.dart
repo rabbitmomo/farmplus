de47e67488479cfde963ae171928f08f9068d6be
LING YU QIAN <149683178+lingyuqian0301@users.noreply.github.com>
2024-03-17 23:00:58 +0800

    Update crops-suggestion-jfd.dart

--- lib/page-1/crops-suggestion-jfd.dart
+++ lib/page-1/crops-suggestion-jfd.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/utils.dart';
 
-class Scene extends StatelessWidget {
-  const Scene({super.key});
+class Scene3 extends StatelessWidget {
+  const Scene3({super.key});
 
   @override
   Widget build(BuildContext context) {
