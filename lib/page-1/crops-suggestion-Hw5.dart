cf8956b36b30568a5ebf75da730911567553a148
LING YU QIAN <149683178+lingyuqian0301@users.noreply.github.com>
2024-03-17 23:09:52 +0800

    Update crops-suggestion-Hw5.dart

--- lib/page-1/crops-suggestion-Hw5.dart
+++ lib/page-1/crops-suggestion-Hw5.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/utils.dart';
 
-class Scene extends StatelessWidget {
-  const Scene({super.key});
+class Scene8 extends StatelessWidget {
+  const Scene8({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -771,7 +771,7 @@ class Scene extends StatelessWidget {
     onPressed: () {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => const Scene()),
+        MaterialPageRoute(builder: (context) => const Scene9()),
       );
     },
     child: const Text('Go to Crops Suggestion'),
